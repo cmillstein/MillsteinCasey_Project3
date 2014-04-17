c834642bf0c1e1d480e336c85eaf0a6f3aa0a144
cmillstein <cmillstein@fullsail.edu>
2014-04-17 13:58:32 -0400

    Update Millstein_Casey_Project3.js

--- Millstein_Casey_Project3.js
+++ Millstein_Casey_Project3.js
@@ -3,54 +3,61 @@
 // Project 3
 
 
-var caseyMillstein
-var electricGuitars = ["Les Paul", "SG", "Flying V", "Firebird"]
-var acousticGuitars = ["Jumbo", "Small Body", "Square Shoulder", "Mandolin"]
+// Arrays
+var electricGuitars = ["Les Paul", "SG", "Flying V", "Firebird"];
+var acousticGuitars = ["Jumbo", "Small Body", "Square Shoulder", "Mandolin"];
 
 
-console.log("Time to go to Guitar Center!")
-guitarQuestion = window.prompt('What guitar should I get?', 'Gibson/Fender');
-var gibsonGuitar = procedureFunction(guitarQuestion);
-
-if (gibsonGuitar) {
-    guitarConfirm = window.confirm('Should I get an electric or acoustic?');
-
-
-
+console.log("Its time to go look for a new guitar!");
+guitarQuestion = window.prompt('Should I get an electric guitar?', 'yes/no');
+var buyElectric = procedureFunction(guitarQuestion);
+if (buyElectric) {
+    guitarConfirm = window.confirm('Do you have enough money for a Gibson?');
+}
+if (guitarQuestion === true){
+	console.log("Lets go get that electric guitar!");
+	
+}
 
 // JSON
 
 var jsonGuitar = {
 	"guitars": [
 		{
-			"brand": Gibson,
-			"model": Les Paul,
-			"type": Electric,
+			"brand": "Gibson",
+			"model": "Les Paul",
+			"type": "Electric",
 			"cost over $2500.00?": true
 		},
 		{
-			"brand(2)": Gibson,
-			"model": SG,
-			"type": Electric,
+			"brand2": "Gibson",
+			"model": "SG",
+			"type": "Electric",
 			"cost over $2500.00?": true
 
 		},
 		{
-			"brand(3)": Fender,
-			"model": Stratocaster,
-			"type": Electric,
+			"brand3": "Fender",
+			"model": "Stratocaster",
+			"type": "Electric",
 			"cost over $2500.00?": true
 		},
 		{
-			"brand(4)": Yamaha,
-			"model": APX500,
-			"type": Acoustic,
+			"brand4": "Yamaha",
+			"model": "APX500",
+			"type": "Acoustic",
 			"cost over $2500.00?": false
 		},
 		{
-			"brand(5)": Ibanez,
-			"model": Prestige,
-			"type": Electric,
+			"brand5": "Ibanez",
+			"model": "Prestige",
+			"type": "Electric",
 			"cost over $2500.00?": false
+		}
 	]
 };
+
+function booleanFunction(isElectric,data){
+    
+    if(data.)
+}
